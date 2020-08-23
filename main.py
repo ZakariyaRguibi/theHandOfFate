96197a3c10d4ef965ee32712c5f6af96e405e82f
zarax <zarax@email.com>
2020-08-23 23:53:43 +0000

    release : adding commands

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# bot.py
+# TheHandOfFate
 import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from util import * 
 load_dotenv()
 
 #get discord token from .env folder
@@ -23,12 +24,32 @@ async def help(ctx):
         colour = discord.Colour.green())
     embed.set_author(name='Help : list of commands available')
     embed.add_field(name='$lickass', value='complimenting the one in charge for xp', inline=False)
+    embed.add_field(name='$setDm', value='set the dm  ', inline=False)
+    embed.add_field(name='$dm', value='whisper to a player', inline=False)
     await ctx.send(embed=embed)
 
-@client.command(pass_context=True)
-async def dm(ctx, user: discord.User, *, message=None):
-    message = message or "This Message is sent via DM"
-    await user.send(message)
 
-#starting
+@client.command(pass_context=True)
+async def setDm(ctx, user: discord.User): 
+    strUser = user.id
+    setDM(strUser)
+    await ctx.send(user.mention + " is now The Mighty DM , Time for adventure ")
+
+async def forwardMessage(message,user1,user2):
+    message=user1.mention + " whispers to " + user2.mention + ": " + message
+    dm = client.get_user(getDM())
+    print(dm)
+    await dm.send(message)
+
+@client.command(pass_context=True)
+async def dm(ctx, user: discord.User, *, input_message=None):
+    input_message = input_message or "This Message is sent via DM"
+    message = "whisper from "+ ctx.message.author.mention + " : " + input_message
+    await user.send(message)
+    message = ctx.message
+    await message.delete()
+    await forwardMessage(input_message,ctx.message.author,user)
+
+
+#starting 
 client.run(TOKEN)
---
+++ util.py
@@ -0,0 +1,12 @@
+import json
+
+#get dungeon master
+def getDM():
+        with open('dm.json') as f:
+            data = json.load(f)
+            return data
+
+#set dungeon master
+def setDM(dm):
+        with open('dm.json', 'w') as json_file:
+            json.dump(dm, json_file)
